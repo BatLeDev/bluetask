6d00de75a0c9856a8bb20d52cfd5bdca1e3565e6
BatLeDev <baptiste.guerny@gmail.com>
2024-05-29 14:31:27 +0200

    feat: add about page

--- README.md
+++ README.md
@@ -9,27 +9,33 @@
 [BlueTask](bluetask.batledev.com) is a powerful task management application designed to help you organize and keep track of your tasks efficiently. With a user-friendly interface, BlueTask ensures you never forget an important task.
 
 ## Features
+
 - **Create, Modify, Archive, and Delete Tasks:** Manage your tasks effortlessly with intuitive controls.
 - **User Authentication:** Secure your tasks with a login and registration system.
 - **Task Labels:** Organize tasks with customizable labels.
 - **Trash Bin:** Recover accidentally deleted tasks from the trash bin.
 
 ## Initial Project
+
 BlueTask started as a project for a web development course. The initial requirements were to create a TODO application with various functionalities, such as task description, start and end dates, status, and priority management.
   
 For more details on the initial project, visit: [Course Project](https://matthieu495.gitlab.io/r4.a.10/section/tp4/).
 
 ## Project Expansion
+
 I decided to expand on this project to gain more experience in web development, including:
+
 - **Hosting**
 - **Deployment**
 - **Database management**
 
 Choosing **Vuetify** for the CSS framework was a strategic decision because:
+
 - I had prior experience with Vuetify at [Koumoul](http://https://koumoul.com/), where I used it extensively.
 - It supported my future career plans, as I aimed to secure a work-study program at the same company, necessitating proficiency in Vuetify.
 
 ## TODO
+
 - [ ] **404 Page:** Create a custom 404 page.
 - [ ] **About Page:** Add an about page with project details.
 - [ ] **Long Tasks:** Support tasks with a lot of lines.
@@ -37,18 +43,23 @@ Choosing **Vuetify** for the CSS framework was a strategic decision because:
 - [ ] **Task Sharing:** Share tasks with other users.
 
 ## Technologies Used
+
 - **Firebase Firestore:** For database management
 - **Vue.js:** The main framework.
 - **Vuetify:** For the UI components.
 - **Vuefire:** To integrate Firebase with Vue.js.
 
 ## Installation
+
 1. **Create a Firestore Database:**
+
 - Set up a Firebase project and enable Firestore.
 - Copy the Firebase configuration details for Javascript.
 
 2. **Environment Variables:**
+
 - Create a .env file in the project root with the following variables (given by Firebase)
+
 ```bash
 VUE_APP_FIREBASE_API_KEY=your_api_key
 VUE_APP_FIREBASE_AUTH_DOMAIN=your_project_id.firebaseapp.com
@@ -59,20 +70,24 @@ VUE_APP_FIREBASE_APP_ID=your_app_id
 ```
 
 3. **Run in Development Mode:**
+
 ```bash
 npm i
 npm run dev
 ```
 
 4. **Generate Production Files:**
+
 ```bash
 npm run build
 ```
 
 ## Screenshots
+
 ...coming soon
 
 ## Credits
+
 - **Design:** Inspired by this [Figma design](https://www.figma.com/community/file/1336799126437101680) and [Google Keep](https://keep.google.com/u/0/#home).
 - **Illustrations:** Provided by [Storyset](https://storyset.com).
 
--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,48 +1,133 @@
 <template>
   <v-container>
-    <h1 class="display-2 text-center my-4">Contactez-nous</h1>
+    <h1 class="display-2 text-center my-4">À Propos de BlueTask</h1>
 
     <v-row justify="center">
       <v-col
         cols="12"
         md="8"
       >
+        <!-- Logo BlueTask -->
+        <v-img
+          src="https://github.com/BatLeDev/bluetask/blob/master/src/assets/logo.png"
+          alt="BlueTask Logo"
+          class="my-4 mx-auto d-block"
+          max-width="200"
+        ></v-img>
+
+        <!-- Présentation du Projet -->
         <p class="lead text-center mb-10">
-          Vous avez une question, un problème ou une suggestion ? N'hésitez pas à nous contacter !
+          Bienvenue sur <strong>BlueTask</strong>, votre application de gestion de tâches puissante et intuitive, conçue
+          pour vous aider à organiser et suivre vos tâches efficacement. Avec son interface conviviale, BlueTask vous
+          assure de ne jamais oublier une tâche importante.
         </p>
 
-        <v-row class="mx-2">
-          <v-col>
-            <h2 class="text-center mb-4">Signaler une erreur</h2>
-            <p class="mb-2">
-              Vous avez rencontré une erreur ou un problème avec notre service ? n'hésitez pas à nous le
-              signaler en nous envoyant un e-mail à l'adresse suivante :
-              <a
-                class="link"
-                href="mailto:baptiste.guerny@gmail.com?subject=[BlueTask] J'ai trouvé une erreur !&body=Bonjour M.GUERNY,%0D%0A"
-              >
-                baptiste.guerny@gmail.com
-              </a>
+        <!-- Origine et Évolution du Projet -->
+        <v-row class="mx-2 my-4">
+          <v-col cols="12">
+            <h2 class="text-center mb-4">Origine et Évolution du Projet</h2>
+            <v-img
+              src="https://storyset.com/illustration/startup/startup"
+              alt="Projet Initial"
+              class="mb-4 mx-auto d-block"
+              max-width="400"
+            ></v-img>
+            <p>
+              BlueTask a commencé comme un projet pour un cours de développement web. Les exigences initiales étaient de
+              créer une application TODO avec diverses fonctionnalités telles que la description des tâches, les dates
+              de début et de fin, la gestion des statuts et des priorités.
             </p>
-          </v-col>
-
-          <v-col>
-            <h2 class="text-center mb-4">Questions et suggestions</h2>
-            <p class="mb-2">
-              Pour toute question, suggestion ou autres demandes, n'hésitez pas à me contacter
-              par e-mail. Je serai ravi de vous lire !
-            </p>
-            <p class="text-center">
-              <strong>Mail de contact :</strong>
-              <a
-                class="link"
-                href="mailto:baptiste.guerny@gmail.com"
-              >
-                baptiste.guerny@gmail.com
-              </a>.
+            <p>
+              J'ai décidé d'élargir ce projet pour acquérir plus d'expérience en développement web, y compris
+              l'hébergement, le déploiement, et la gestion de base de données. Le choix de <strong>Vuetify</strong> pour
+              le framework CSS a été une décision stratégique en raison de mon expérience antérieure chez <a
+                href="http://https://koumoul.com/"
+                target="_blank"
+              >Koumoul</a>.
             </p>
           </v-col>
         </v-row>
+
+        <!-- Qui suis-je ? -->
+        <v-row class="mx-2 my-4">
+          <v-col cols="12">
+            <h2 class="text-center mb-4">Qui suis-je ?</h2>
+            <v-img
+              src="https://storyset.com/illustration/profile/profile"
+              alt="Mon Profil"
+              class="mb-4 mx-auto d-block"
+              max-width="400"
+            ></v-img>
+            <p>
+              Je suis Baptiste Guerny, un développeur web passionné avec une expérience dans le développement
+              d'applications intuitives et efficaces. J'ai utilisé des technologies comme Vue.js, Vuetify et Firebase
+              pour créer BlueTask, en appliquant les compétences acquises lors de mes études et expériences
+              professionnelles.
+            </p>
+          </v-col>
+        </v-row>
+
+        <!-- Contactez-nous -->
+        <v-row class="mx-2 my-4">
+          <v-col cols="12">
+            <h2 class="text-center mb-4">Contactez-moi</h2>
+            <v-img
+              src="https://storyset.com/illustration/communication/communication"
+              alt="Contactez-nous"
+              class="mb-4 mx-auto d-block"
+              max-width="400"
+            ></v-img>
+            <v-row>
+              <v-col
+                cols="12"
+                md="6"
+              >
+                <h3 class="text-center mb-4">Signaler une Erreur</h3>
+                <p>
+                  Vous avez rencontré une erreur ou un problème avec notre service ? N'hésitez pas à nous le signaler en
+                  nous envoyant un e-mail à l'adresse suivante :
+                  <a
+                    href="mailto:baptiste.guerny@gmail.com?subject=[BlueTask] J'ai trouvé une erreur !&body=Bonjour M.GUERNY,%0D%0A">baptiste.guerny@gmail.com</a>.
+                </p>
+              </v-col>
+              <v-col
+                cols="12"
+                md="6"
+              >
+                <h3 class="text-center mb-4">Questions et Suggestions</h3>
+                <p>
+                  Pour toute question, suggestion ou autre demande, n'hésitez pas à me contacter par e-mail. Je serai
+                  ravi de vous lire !
+                </p>
+                <p class="text-center">
+                  <strong>Mail de contact :</strong>
+                  <a href="mailto:baptiste.guerny@gmail.com">baptiste.guerny@gmail.com</a>.
+                </p>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+
+        <!-- Crédits -->
+        <v-row class="mx-2 my-4">
+          <v-col cols="12">
+            <h2 class="text-center mb-4">Crédits</h2>
+            <p>
+              <strong>Design:</strong> Inspiré par ce <a
+                href="https://www.figma.com/community/file/1336799126437101680"
+                target="_blank"
+              >design Figma</a> et <a
+                href="https://keep.google.com/u/0/#home"
+                target="_blank"
+              >Google Keep</a>.<br>
+              <strong>Illustrations:</strong> Fournies par <a
+                href="https://storyset.com"
+                target="_blank"
+              >Storyset</a>.
+            </p>
+          </v-col>
+        </v-row>
+
       </v-col>
     </v-row>
   </v-container>
